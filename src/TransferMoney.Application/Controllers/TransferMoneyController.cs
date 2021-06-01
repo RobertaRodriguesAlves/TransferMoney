284e4ccac289c32661306de0525f8358771b80b0
Roberta Suélen Rodrigues Alves <robertasrodriguealves@gmail.com>
2021-06-01 20:16:59 -0300

    Setting up

---
+++ src/TransferMoney.Application/Controllers/TransferMoneyController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransferMoney.Application.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TransferMoneyController : ControllerBase
+    {
+
+    }
+}
